373e0711f394007354bfa81a5facf0fc7338904f
wsx <wushuixiu@jxnu.edu.cn>
2021-08-02 09:41:34 +0800

    增加了一个Test类

---
+++ src/main/java/edu/jxnu/Test.java
@@ -0,0 +1,23 @@
+package edu.jxnu;
+
+public class Test {
+     private int ID;
+     private String value;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "Test [ID=" + ID + ", value=" + value + "]";
+	}
+     
+}
